301792986209ba027302d453d5b7895a5569ed4e
Ghushit Kumar Chutia <ghushit45@gmail.com>
2026-02-07 23:44:24 +0530

    fix: add MONGODB_URI guard in db config

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,11 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  if (!process.env.MONGODB_URI) {
+    console.error("MONGODB_URI is not defined in environment variables");
+    process.exit(1);
+  }
+
   try {
     const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
